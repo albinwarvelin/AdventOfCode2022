cddb068a8a26809570785d53a74f078192dc603d
albinwarvelin <albin.warvelin@gmail.com>
2022-12-03 14:31:42 +0100

    Day 3

---
+++ Day3.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day3 : AdventOfCode
+    {
+        public override void Run1(string[] input)
+        {
+            string items = "";
+
+            foreach(string rucksack in input)
+            {
+                string compartment1 = rucksack.Substring(0, rucksack.Length / 2);
+                string compartment2 = rucksack.Substring(rucksack.Length / 2);
+
+                foreach(char item in compartment1)
+                {
+                    if (compartment2.Contains(item))
+                    {
+                        items += item;
+                        break;
+                    }
+                }
+            }
+
+            int priority = 0;
+
+            foreach(char item in items)
+            {
+                if(char.IsUpper(item))
+                {
+                    priority += item - 38;
+                }
+                else
+                {
+                    priority += item - 96;
+                }
+            }
+
+            Console.WriteLine(priority);
+        }
+
+        public override void Run2(string[] input)
+        {
+            string badges = "";
+
+            for(int i = 0; i < input.Length; i += 3)
+            {
+                foreach(char item in input[i])
+                {
+                    if(input[i + 1].Contains(item) && input[i + 2].Contains(item))
+                    {
+                        badges += item;
+                        break;
+                    }
+                }
+            }
+
+            int priority = 0;
+
+            foreach (char item in badges)
+            {
+                if (char.IsUpper(item))
+                {
+                    priority += item - 38;
+                }
+                else
+                {
+                    priority += item - 96;
+                }
+            }
+
+            Console.WriteLine(priority);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,10 +10,10 @@ namespace AdventOfCode2022
     {
         static void Main()
         {
-            string[] input = File.ReadAllLines("inputs/day2.txt");
+            string[] input = File.ReadAllLines("inputs/day3.txt");
 
 
-            AdventOfCode adventOfCode = new Day2();
+            AdventOfCode adventOfCode = new Day3();
 
             adventOfCode.Run1(input);
             adventOfCode.Run2(input);
